197b51279d9d99c1f068f89e0ec775356a0881a2
Eric <126840543+eccol@users.noreply.github.com>
2023-10-14 17:28:10 -0400

    Test post validation

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:poster) { User.create!(email: "c@c.c", password: "123456") }
+
+  it "is valid with valid attributes" do
+    post = poster.posts.build(content: "This post should be fine.")
+    post.save
+    expect(post).to be_valid
+  end
+
+  it "cannot be blank" do
+    post = poster.posts.build
+    post.save
+    expect(post).not_to be_valid
+  end
 end
