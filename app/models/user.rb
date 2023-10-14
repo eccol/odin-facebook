4a2c1bcc523661e27809c5cf48f036338c0ae189
Eric <126840543+eccol@users.noreply.github.com>
2023-10-14 17:20:04 -0400

    Post skeleton

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :sent_friend_requests, foreign_key: "sender_id", class_name: "FriendRequest"
   has_many :received_friend_requests, foreign_key: "recipient_id", class_name: "FriendRequest"
 
+  has_many :posts
+
   def friends
     (FriendRequest.where(sender: self).where(accepted: true).map(&:recipient) +
       FriendRequest.where(recipient: self).where(accepted: true).map(&:sender))
