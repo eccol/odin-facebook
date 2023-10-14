1c8af5b0e216e6e7e9dd9119531348fe3141b187
Eric <126840543+eccol@users.noreply.github.com>
2023-10-14 16:27:25 -0400

    Tests for User#friends

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "#friends" do
+    it "returns accepted friend requests" do
+      sender = User.create
+      recipient = User.create
+      request = FriendRequest.create(sender: sender, recipient: recipient)
+      request.accept
+      expect(sender.friends).to include(recipient)
+    end
+
+    it "does not return unaccepted requests" do
+      sender = User.create
+      recipient = User.create
+      request = FriendRequest.create(sender: sender, recipient: recipient)
+      expect(sender.friends).to include(recipient)
+    end
+  end
 end
