8d50bb754dc93ae575dad01cd232b35ec0cc78f5
Eric <126840543+eccol@users.noreply.github.com>
2023-10-14 16:02:56 -0400

    Pass test: Users can send requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :sent_friend_requests, foreign_key: "sender_id", class_name: "FriendRequest"
+  has_many :received_friend_requests, foreign_key: "recipient_id", class_name: "FriendRequest"
 end
