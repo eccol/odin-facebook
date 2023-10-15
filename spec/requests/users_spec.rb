2869f95a3d8e49d8117d44e0fb08ce3230d28228
Eric <126840543+eccol@users.noreply.github.com>
2023-10-15 14:39:24 -0400

    Require login for all pages

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe "Users", type: :feature do
   describe "GET /index" do
     it "displays a list of users" do
-      user = User.create!(email: "a@a.a", password: "123456").build_profile(name: "Superman")
+      user = User.create!(email: "a@a.a", password: "123456")
+      user.build_profile(name: "Superman")
       user.save
+      login_as(user)
       visit users_path
       expect(page).to have_text('Superman')
     end
