eae8c6f173cbfda23f8fc797239b0d1b72d35ff3
Eric <126840543+eccol@users.noreply.github.com>
2023-10-15 13:47:54 -0400

    User index displays names instead of emails

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :feature do
   describe "GET /index" do
-    xit "displays a list of users" do
-      pending 'Rewrite this test after creating profile information'
-      User.create!(email: "a@a.a", password: "123456")
+    it "displays a list of users" do
+      user = User.create!(email: "a@a.a", password: "123456").build_profile(name: "Superman")
+      user.save
       visit users_path
-      expect(page).to have_text('a@a.a')
+      expect(page).to have_text('Superman')
     end
   end
 end
