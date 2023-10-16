1bf09fd180037b2e6b89451f3571f442eda24024
Eric <126840543+eccol@users.noreply.github.com>
2023-10-15 21:02:44 -0400

    Set up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+10.times do
+  user = User.create(
+    email: Faker::Internet.email,
+    password: "123456"
+  )
+  user.build_profile(
+    name: Faker::Name.name,
+    birthday: Faker::Date.between(from: 60.years.ago, to: 18.years.ago),
+    city: Faker::Address.city
+  )
+  user.save
+end
