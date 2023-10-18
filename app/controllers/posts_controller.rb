c5aa52367693ff21743b0a06eed9ec4e0b8cee18
Eric <126840543+eccol@users.noreply.github.com>
2023-10-17 20:10:02 -0400

    Add new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,27 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc).all
     @friends = current_user.friends
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.turbo_stream
+      else
+        format.html {render :new, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
-# RSpec.describe "Posts", type: :request do
-#   describe "GET /index" do
-#     pending "add some examples (or delete) #{__FILE__}"
-#   end
-# end
+RSpec.describe "Posts", type: :feature do
+  context "when making a post" do
+    it "appears" do
+      Capybara.current_driver = :selenium_chrome_headless
+      user = User.create!(email: "a@a.a", password: "123456")
+      user.build_profile(name: "Superman")
+      user.save
+      login_as(user)
+      visit root_path
+      fill_in 'post[content]', with: 'abcxyz'
+      click_button 'Post'
+      expect(page).to have_content('abcxyz')
+    end
+
+    it "appears on the same page" do
+      Capybara.current_driver = :selenium_chrome_headless
+      user = User.create!(email: "a@a.a", password: "123456")
+      user.build_profile(name: "Superman")
+      user.save
+      login_as(user)
+      visit root_path
+      fill_in 'post[content]', with: 'abcxyz'
+      click_button 'Post'
+      expect(page).to have_current_path(root_path)
+    end
+  end
+end
