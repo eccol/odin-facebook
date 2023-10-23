24c7aa578ad55bb0ffdc5d33b5ebd4630fa211a6
Eric <126840543+eccol@users.noreply.github.com>
2023-10-23 18:41:32 -0400

    Comment tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:post_user) { User.create!(email: "post@post.post", password: "123456", profile_attributes: {name: "Poster"})}
+  let(:comment_user) { User.create!(email: "comment@comment.comment", password: "123456", profile_attributes: {name: "Commenter"})}
+  let(:post) { post_user.posts.new(content: "This is a post.")}
+
+  it "is valid with content" do
+    comment = Post.comments.new(user_id: comment_user.id, content: "Fantabuluous!")
+    expect(comment).to be_valid
+  end
+
+  it "is not valid without content" do
+    comment = Post.comments.new(user_id: comment_user.id)
+    expect(comment).not_to be_valid
+  end
 end
