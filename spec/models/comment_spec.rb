f1b64242d282c7c707435f1b2b43b36d3014b1ab
Eric <126840543+eccol@users.noreply.github.com>
2023-10-23 18:46:45 -0400

    Fix comment tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   let(:post_user) { User.create!(email: "post@post.post", password: "123456", profile_attributes: {name: "Poster"})}
   let(:comment_user) { User.create!(email: "comment@comment.comment", password: "123456", profile_attributes: {name: "Commenter"})}
-  let(:post) { post_user.posts.new(content: "This is a post.")}
+  let(:post) { post_user.posts.create!(content: "This is a post.")}
 
   it "is valid with content" do
-    comment = Post.comments.new(user_id: comment_user.id, content: "Fantabuluous!")
+    comment = post.comments.new(user_id: comment_user.id, content: "Fantabuluous!")
     expect(comment).to be_valid
   end
 
   it "is not valid without content" do
-    comment = Post.comments.new(user_id: comment_user.id)
+    comment = post.comments.new(user_id: comment_user.id)
     expect(comment).not_to be_valid
   end
 end
