068d2569741ae2ee8fba7e0671066bf26b81533c
Eric <126840543+eccol@users.noreply.github.com>
2023-10-24 18:23:18 -0400

    Resize uploaded profile pictures

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,5 @@
 class ProfilesController < ApplicationController
+  before_action lambda { resize_avatar(profile_params[:avatar])}, only: [:update]
   def show
     @user = current_user
   end
@@ -18,5 +19,14 @@ class ProfilesController < ApplicationController
   private
   def profile_params
     params.require(:profile).permit(:name, :birthday, :city, :avatar)
+  end
+
+  def resize_avatar(image_param)
+    return unless image_param
+
+    ImageProcessing::MiniMagick
+    .source(image_param)
+    .resize_to_fit(100,100)
+    .call(destination: image_param.tempfile.path)
   end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,8 +2,4 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_one_attached :avatar
   validates :name, presence: true
-
-  has_one_attached :avatar do |attachable|
-    attachable.variant(resize_to_limit: [100,100])
-  end
 end
